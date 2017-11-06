c863213ab70863869b0b3210cdafd09e42f79b1a
liulei <liulei_work@163.com>
2017-11-06 22:47:50 +0800

    wancheng

--- k.py
+++ k.py
@@ -2,6 +2,8 @@ import urllib.request
 import time
 import pymysql.cursors
 
+
+
 if __name__ == "__main__":
     # 连接数据库
     connect = pymysql.Connect(
@@ -15,6 +17,7 @@ if __name__ == "__main__":
 
     # 获取游标
     cursor = connect.cursor()
+
     while (True):
         # 网址
         url = "http://hq.sinajs.cn/list=hf_OIL"
@@ -29,10 +32,11 @@ if __name__ == "__main__":
 
         # 设置解码方式
         data = data.decode('gb2312')
-
+        print(data)
         # 打印结果
         # print(data)
-        print(data.split('"')[1].split(','))
+        result1 = data.split('"')[1].split(',')
+        print(result1)
 
 
         # 网址
@@ -51,18 +55,21 @@ if __name__ == "__main__":
 
         # 打印结果
         # print(data2)
-        print(data2.split('"')[1].split(','))
+        result2 = data2.split('"')[1].split(',')
+        print(result2)
 
-        sql = "UPDATE testpy SET name = '%s' WHERE id = %d "
-        data = ('adsfads', 1)
-        cursor.execute(sql % data)
+        sql = "UPDATE orlprice SET name = '%s',new_price = '%s',rmb_price ='%s',diff_price = '%s',diff_percent = '%s',open_price = '%s',high_price='%s',low_price = '%s',last_price = '%s',hold_count = '%s',buy_price = '%s',sell_price ='%s',price_time = '%s' WHERE id = %d "
+        data1 = (result2[13],result2[0],"","",result2[1],result2[8],result2[4],result2[5],result2[7],result2[9],result2[2],result2[3],result2[6], 1)
+        data2 = (result1[13],result1[0],"","",result1[1],result1[8],result1[4],result1[5],result1[7],result1[9],result1[2],result1[3],result1[6], 2)
 
-        if cursor.rowcount == 0:
-            sql = "insert into  testpy (name)values( '%s'  ) "
-            data = ('adsfads')
-            cursor.execute(sql % data)
+        cursor.execute(sql % data1)
+        cursor.execute(sql % data2)
+
         connect.commit()
         print('成功修改', cursor.rowcount, '条数据')
 
         connect.commit()
         time.sleep(30)
+
+
+

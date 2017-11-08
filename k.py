1b2ed18af5f17325054626e91ba2b8b7620ccc0e
liulei.bj <liulei.bj@fang.com>
2017-11-08 10:41:07 +0800

    2.0

--- k.py
+++ k.py
@@ -19,33 +19,42 @@ if __name__ == "__main__":
             cursor = connect.cursor()
 
             while (True):
-                url = "http://hq.sinajs.cn/list=hf_"
-                urllist = [url + "OIL", url + 'CL', url + 'DINIW', url + 'USDCNY', url + 'GC', url + 'XAU']
-                count = 1
+                url = "http://hq.sinajs.cn/list="
+                urllist = [[url + "hf_OIL",1], [url + 'hf_CL',2], [url + 'hf_GC',3], [url + 'hf_XAU',4], [url + 'DINIW',5], [url + 'USDCNY',6]]
                 for url in urllist:
-                    #    请求
-                    request = urllib.request.Request(url)
+                    try:
+                        #    请求
+                        num = url[1]
+                        request = urllib.request.Request(url[0])
 
-                    # 爬取结果
-                    response = urllib.request.urlopen(request)
+                        # 爬取结果
+                        response = urllib.request.urlopen(request)
 
-                    data = response.read()
+                        data = response.read()
 
-                    # 设置解码方式
-                    data = data.decode('gb2312')
-                    print(data)
-                    # 打印结果
-                    result = data.split('"')[1].split(',')
-                    print(result)
-                    sql = "UPDATE orlprice SET name = '%s',new_price = '%s',rmb_price ='%s',diff_price = '%s',diff_percent = '%s',open_price = '%s',high_price='%s',low_price = '%s',last_price = '%s',hold_count = '%s',buy_price = '%s',sell_price ='%s',price_time = '%s' WHERE id = %d "
-                    param = (
-                        result[13], result[0], "", "", result[1], result[8], result[4], result[5], result[7],
-                        result[9],
-                        result[2], result[3], result[6], count)
-                    cursor.execute(sql % param)
-                    connect.commit()
-                    count +=1
-                    print('成功修改', cursor.rowcount, '条数据')
+                        # 设置解码方式
+                        data = data.decode('gb2312')
+                        print(data)
+                        # 打印结果
+                        result = data.split('"')[1].split(',')
+                        print(result)
+                        sql = "UPDATE orlprice SET name = '%s',new_price = '%s',rmb_price ='%s',diff_price = '%s',diff_percent = '%s',open_price = '%s',high_price='%s',low_price = '%s',last_price = '%s',hold_count = '%s',buy_price = '%s',sell_price ='%s',price_time = '%s' WHERE id = %d "
+                        if(num<=4):
+                            param = (
+                                result[13], result[0], "", "", result[1], result[8], result[4], result[5], result[7],
+                                result[9],
+                                result[2], result[3], result[6], num)
+                        else:
+                            param = (
+                                result[9], result[8], "", "", "", result[3], result[6], '', result[5],
+                                result[4],
+                                result[1], result[2], result[0], num)
+
+                        cursor.execute(sql % param)
+                        connect.commit()
+                        print('成功修改', cursor.rowcount, '条数据')
+                    except Exception as e:
+                        print(e)
 
                 time.sleep(30)
         except Exception as e:
